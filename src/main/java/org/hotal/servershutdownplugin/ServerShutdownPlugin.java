5585114e8e3d98f6e793550ad0c597f41a87dc18
蛍 <93361361+minamikana-git@users.noreply.github.com>
2023-03-17 21:46:13 +0900

    master

--- src/main/java/org/hotal/servershutdownplugin/ServerShutdownPlugin.java
+++ src/main/java/org/hotal/servershutdownplugin/ServerShutdownPlugin.java
@@ -1,17 +1,48 @@
 package org.hotal.servershutdownplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ServerShutdownPlugin extends JavaPlugin {
+import java.util.Calendar;
+
+public class ServerShutdownPlugin extends JavaPlugin {
+
+    // 設定ファイルから取得する時間
+    private int shutdownHour;
+    private int shutdownMinute;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // config.ymlファイルがなければ生成する
+        saveDefaultConfig();
 
+        // config.ymlファイルから設定を読み込む
+        shutdownHour = getConfig().getInt("shutdown.hour");
+        shutdownMinute = getConfig().getInt("shutdown.minute");
+
+        // 毎分システム時刻をチェックするタスクを開始する
+        Bukkit.getScheduler().runTaskTimer(this, new Runnable() {
+            @Override
+            public void run() {
+                checkShutdownTime();
+            }
+        }, 0L, 20L * 60L);
     }
 
+    // プラグインが無効化されたときに呼ばれる
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // プラグインが停止されるときに何かする場合はここに書く
+    }
+
+    // システム時刻が停止時間になっているかをチェックする
+    private void checkShutdownTime() {
+        Calendar cal = Calendar.getInstance();
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        int minute = cal.get(Calendar.MINUTE);
+
+        if (hour == shutdownHour && minute == shutdownMinute) {
+            Bukkit.getServer().shutdown();
+        }
     }
 }
